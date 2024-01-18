a968c9446ff6ce19fa743b7e5d5e6b14beb4b499
yumenome <waiyan2015.wy65@gmail.com>
2024-01-18 22:46:10 +0630

    test

---
+++ app/Http/Controllers/TestingController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+
+    public function direct_method(Request $request)
+    {
+
+        $validator = validator($request->all(), [
+            'total' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return ($validator->errors());
+        }
+
+
+        $first_num = rand(1, 9);
+        $possible_result = [];
+        $questions = [$first_num];
+
+
+        for($i = 0; $i < ($request->total -1); $i++){
+            switch ($first_num) {
+                case 0:
+                    $possible_result = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                    break;
+                case 1:
+                    $possible_result = [1, 2, 3, 5, 6, 7, 8, -1];
+                    break;
+                case 2:
+                    $possible_result = [1, 2, 5, 6, 7, -1, -2];
+                    break;
+                case 3:
+                    $possible_result = [1, 5, 6, -1, -2, -3];
+                    break;
+                case 4:
+                    $possible_result = [5, -1, -2, -3, -4];
+                    break;
+                case 5:
+                    $possible_result = [1, 2, 3, 4, -5];
+                    break;
+                case 6:
+                    $possible_result = [1, 2, 3, -1, -5, -6];
+                    break;
+                case 7:
+                    $possible_result = [1, 2, -1, -2, -5, -6, -7];
+                    break;
+                case 8:
+                    $possible_result = [1, -1, -2, -3, -5, -6, -7, -8];
+                    break;
+                case 9:
+                    $possible_result = [-1, -2, -3, -4, -5, -6, -7, -8, -9];
+                    break;
+            };
+            $random_value = array_rand($possible_result);
+            $question = $possible_result[$random_value];
+            array_push($questions, $question);
+
+            $first_num = array_reduce($questions, function ($a, $b) {return $a + $b;});
+        }
+        $answer = array_sum($questions);
+        return ['questions' => $questions,'answer' => $answer];
+    }
+
+
+}
